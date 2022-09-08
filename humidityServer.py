29ef27c3eaa7ce48f680a6cc37cbd0324111d5bb
Brian Moody <brian.kmdy@gmail.com>
2022-09-07 19:39:56 -0700

    Update timings in the server

--- humidityServer.py
+++ humidityServer.py
@@ -2,8 +2,12 @@ import flask
 import sqlite3
 import datetime
 import json
+import time
 
 app = flask.Flask(__name__)
+
+granularity = 180
+history = 3 * 24 * 60 * 60
 
 @app.route('/')
 def home():
@@ -11,8 +15,11 @@ def home():
 
 @app.route('/data')
 def data():
+    global granularity
+    global history
+
     con = sqlite3.connect('humidity.db')
-    response = json.dumps([row for row in con.cursor().execute('SELECT * from humidity')])
+    response = json.dumps([row for row in con.cursor().execute('SELECT * from humidity WHERE epoch % ? = 0 AND epoch > ?', (granularity, time.time() - history))])
     con.close()
     return response
 
