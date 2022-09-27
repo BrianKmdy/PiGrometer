3e757a35de0d13257191ca55f84ddc7dafdd13d4
Brian Moody <brian.kmdy@gmail.com>
2022-09-26 23:39:47 -0700

    Add params for server to set granularity and history

--- pigrometer/server.py
+++ pigrometer/server.py
@@ -1,27 +1,38 @@
 import flask
 import sqlite3
-import datetime
 import json
 import time
+import os
+import threading
+from flask import request
 
 app = flask.Flask(__name__)
 
-granularity = 900
-history = 3 * 24 * 60 * 60
-
 @app.route('/')
 def home():
-    return flask.render_template('chart.html')
+    granularity = request.args.get('granularity', default=900, type=int)
+    history = request.args.get('history', default=(3 * 24 * 60 * 60), type=int)
+
+    return flask.render_template('chart.html', granularity=granularity, history=history)
 
 @app.route('/data')
 def data():
-    global granularity
-    global history
+    granularity = request.args.get('granularity', type=int)
+    history = request.args.get('history', type=int)
 
     con = sqlite3.connect(os.path.join(os.path.expanduser("~"), '.pigrometer', 'pigrometer.db'))
     response = json.dumps([row for row in con.cursor().execute('SELECT * from humidity WHERE epoch % ? = 0 AND epoch > ?', (granularity, time.time() - history))])
     con.close()
     return response
+
+# class Server(threading.Thread):
+#     def __init__(self):
+#         super().__init__()
+#         self.terminate = threading.Event()
+
+    
+
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
