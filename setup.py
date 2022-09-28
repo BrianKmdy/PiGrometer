5e4331891628a03bd32a308911e13d44a2432efa
Brian Moody <brian.kmdy@gmail.com>
2022-09-27 20:44:15 -0700

    Try passing git ref and git commit to setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,14 @@ import setuptools
 from distutils.core import setup
 import os
 
+GIT_REF = os.environ.get('PIGROMETER_VERSION')
+GIT_COMMIT = os.environ.get('PIGROMETER_VERSION')
+print(GIT_REF)
+print(GIT_COMMIT)
+
 setup(
     name='pigrometer',
-    version=os.environ.get('PIGROMETER_VERSION', 'no_version'),
+    version='no_version',
     description='A webapp for monitoring temperature and humidity with a raspberry pi',
     author='Brian Moody',
     url='https://github.com/BrianKmdy/TempMonitor',
