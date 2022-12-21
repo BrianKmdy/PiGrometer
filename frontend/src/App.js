a0efee9e89dd84826cf23d3065d7f4d6195900c1
Boyi Qu <boyiqu9596@gmail.com>
2022-12-20 23:45:36 -0800

    updated App.js using url from config.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import API from './config.js';
+import config from './config.js';
 import logo from './logo.svg';
 import React, {useEffect} from 'react';
 import axios from 'axios';
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   useEffect(()=>{
-    axios.get(`http://${API}:5000/data`)
+    axios.get(`http://${config.API}:5000/data`)
     .then(({data})=>{
       console.log(data)
     })
