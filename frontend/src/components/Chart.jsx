a2c78491e0ae4c1e4c01f802fab9b032d543771f
Boyi Qu <boyiqu9596@gmail.com>
2023-01-03 17:55:06 -0800

    added LineChart to Chart component

--- frontend/src/components/Chart.jsx
+++ frontend/src/components/Chart.jsx
@@ -14,10 +14,38 @@ const Chart = ({rawData}) => {
   const data = transform(rawData)
   console.log(data)
   return (
-    <div>
-      This is Chart
-      {rawData[1][2]}
-    </div>
+    <LineChart
+      width={800}
+      height={500}
+      data={data}
+      margin={{
+        top: 10,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3"/>
+      <XAxis dataKey="time" />
+      <YAxis yAxisId="left"/>
+      <YAxis yAxisId="right" orientation="right" />
+      <Tooltip />
+      <Legend />
+      <Line 
+        yAxisId="left"
+        type="monotone"
+        dataKey="temperature"
+        stroke="#FF7B54"
+        activeDot={{r:8}}
+      />
+      <Line 
+        yAxisId="left"
+        type="monotone"
+        dataKey="humidity"
+        stroke="#243763"
+        activeDot={{r:8}}
+      />
+    </LineChart>
   )
 }
 
