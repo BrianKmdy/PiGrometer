76722a229f24bd2392229ac7a2cc9bb28e35ad26
Boyi Qu <boyiqu9596@gmail.com>
2023-01-03 17:55:31 -0800

    modified transform function to reflect local time instead of UTC

--- frontend/src/helpers/transform.js
+++ frontend/src/helpers/transform.js
@@ -2,7 +2,10 @@ const transform = (dataSet) => {
     let result = [];
     for (let data of dataSet) {
         let newData = {};
-        newData.time = data[0];
+        let date = new Date(data[0] * 1000)
+        let newDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
+        let time = newDate.toISOString()
+        newData.time = time.slice(11, 16)
         newData.humidity = data[1];
         newData.temperature = data[2];
         result.push(newData);
