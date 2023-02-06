bb399c365257e37e73ecfe253d55848408acddcc
Boyi Qu <boyiqu9596@gmail.com>
2023-02-06 14:58:52 -0800

    modified the transform helper function

--- frontend/src/helpers/transform.js
+++ frontend/src/helpers/transform.js
@@ -5,7 +5,11 @@ const transform = (dataSet) => {
         let date = new Date(data[0] * 1000)
         let newDate = new Date(date.getTime() - date.getTimezoneOffset() * 60000)
         let time = newDate.toISOString()
-        newData.time = Number(time.slice(11, 13) + '.' + time.slice(14, 16))
+        newData.raw = data[0]
+        newData.originalTime = time
+        newData.year = newDate.getFullYear()
+        //newData.time = Number(time.slice(11, 13) + '.' + time.slice(14, 16))
+        newData.time = date
         newData.humidity = data[1];
         newData.temperature = data[2];
         result.push(newData);
